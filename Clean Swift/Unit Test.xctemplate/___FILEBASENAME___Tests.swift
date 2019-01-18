52b0baf070b95198266ed37557e50cd487a8102f
Andrey Chirkov <andrey.chirkov@mac40.Dlink>
2019-01-18 23:16:36 +0300

    CleanSwift Xcode templates

---
+++ Clean Swift/Unit Test.xctemplate/___FILEBASENAME___Tests.swift
@@ -0,0 +1,35 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+@testable import ___PROJECTNAMEASIDENTIFIER___
+import XCTest
+
+class ___VARIABLE_sceneName___Tests: XCTestCase {
+    // MARK: Subject under test
+    var sut: ___VARIABLE_sceneName___!
+    
+    // MARK: Test lifecycle
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // MARK: Test doubles
+    
+    // MARK: Tests
+    func testDoSomething() {
+        // Given
+        
+        // When
+        
+        // Then
+    }
+}
