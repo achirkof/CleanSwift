5c2b4c27d134c736c71c841032e2146e2dd0b7c2
Andrey <achirkof@gmail.com>
2020-12-27 14:58:31 +0300

    Remove ___ORGANIZATIONNAME___ from templates

--- Clean Swift/Unit Tests.xctemplate/___FILEBASENAME___PresenterTests.swift
+++ Clean Swift/Unit Tests.xctemplate/___FILEBASENAME___PresenterTests.swift
@@ -3,7 +3,7 @@
 //  ___PROJECTNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//  Copyright (c) ___YEAR___ 
 //
 
 @testable import ___PROJECTNAMEASIDENTIFIER___
